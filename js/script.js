5db2adc7fd37eaf70716e57d83fd393334129760
Eugene Molari <eugelogic@gmail.com>
2019-12-04 18:58:35 +0000

    Add cards array with images

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// Card Data
+const cardsArray = [
+    {
+        name: 'louise',
+        img: 'img/louise.png',
+    },
+    {
+        name: 'lisa',
+        img: 'img/lisa.jpg',
+    },
+    {
+        name: 'kelly',
+        img: 'img/kelly.png',
+    },
+    {
+        name: 'kerry',
+        img: 'img/kerry.png',
+    },
+    {
+        name: 'chris',
+        img: 'img/chris.jpg',
+    },
+    {
+        name: 'marcus',
+        img: 'img/marcus.jpg',
+    },
+    {
+        name: 'steve',
+        img: 'img/steve.png',
+    },
+    {
+        name: 'russell',
+        img: 'img/russell.jpg',
+    },
+    {
+        name: 'eugene',
+        img: 'img/eugene.png',
+    },
+    {
+        name: 'david',
+        img: 'img/david.png',
+    },
+    {
+        name: 'mark',
+        img: 'img/mark.png',
+    },
+    {
+        name: 'calum',
+        img: 'img/calum.jpg',
+    },
+    {
+        name: 'emily',
+        img: 'img/emily.png',
+    },
+    {
+        name: 'pets',
+        img: 'img/pets.jpg',
+    },
+]
